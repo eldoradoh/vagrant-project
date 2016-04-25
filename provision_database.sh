51f4c510ed8f6cb54431204eaa295d212a788d05
eldoradoh <elhelh28@gmail.com>
2016-04-25 06:42:23 +0200

    Install database script

--- provision_database.sh
+++ provision_database.sh
@@ -8,7 +8,7 @@ apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 # Add MongoDB APT repository url in /etc/apt/sources.list.d/mongodb.list
 echo "deb http://repo.mongodb.org/apt/debian ""wheezy""/mongodb-org/3.2 main" | sudo tee /etc/apt/sources.list.d/mongodb.list
 # Update system with new added repository
-apt-get update
+apt-get update -y
 # Install MongoDB system
 apt-get install -y mongodb-org
 # Restart/Start MongoDB service
